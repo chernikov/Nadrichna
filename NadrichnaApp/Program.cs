de2bb2e569e42bc44026451b332b99d0703d6648
Andriy Chernikov <chernikov@gmail.com>
2020-12-17 11:56:16 +0200

    Create our beatiful solution

---
+++ NadrichnaApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using NadrichnaCore;
+using System;
+
+namespace NadrichnaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car volgaAT2929KH;
+            volgaAT2929KH = new Car(ColorEnum.Black);
+
+            volgaAT2929KH.Move();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NadrichnaCore/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NadrichnaCore
+{
+    public class Vehicle
+    {
+        public ColorEnum Color;
+
+
+        public Vehicle(ColorEnum color)
+        {
+            this.Color = color;
+        }
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"{Color}. I'm moving...");
+        }
+    }
+}
